5d9b1dad7df1764d9e7e72e01fba949fc666c655
April Herwig <aprillherwig@gmail.com>
2025-03-25 15:50:25 +0100

    phase space plots

--- src/data_analysis.jl
+++ src/data_analysis.jl
@@ -2,18 +2,20 @@ using Plots
 using DataFrames, CSV
 
 
-landau = true
+conditions = :twostream
 
-ranks = [7]
+rank = 10
+ranks = [10]
 #ranks = [5, 7, 9, 11, 13]
+ms = [1, 2, 3, 5]
 
 dataframes = [
     CSV.read(
         #"./data/evolution_two_stream_rank_min_5_max_16_step_0d0005.csv",
-        "./data/evolution_$(landau ? "landau" : "two_stream")_rank_min_$(rank)_max_$(rank)_conserved_5_step_0d0005.csv",
+        "./data/evolution_$(string(conditions))_rank_min_$(rank)_max_$(rank)_conserved_$(m)_step_0d0005.csv",
         DataFrame
     )
-    for rank in ranks
+    for m in ms
 ]
 
 observables = names(dataframes[1])[2:end-1]      # 1st col is time and last col is rank
@@ -23,15 +25,15 @@ data_end = 30
 
 evolution_plots = map(observables) do observable
     p = plot(size=(900,600), xlabel="time (step = 0.0005)", ylabel=observable)
-    for ind in eachindex(ranks)
+    for ind in eachindex(ms)
         df = dataframes[ind]
         df = df[data_start .< df[!, "time"] .< data_end, :]
-        rank = ranks[ind]
+        m = ms[ind]
         plot!(p, 
             df[!, "time"], df[!, observable],
-            label="r = $rank",
+            label="m = $m",
             #label="",
-            color=df[:, "rank"],
+            color=ind,
             linewidth=3
         )
     end
@@ -62,17 +64,17 @@ error_plots = map(observables) do observable
             ["10⁻¹⁵", "10⁻¹⁰", "10⁻⁵", "10⁻¹", "10⁰"]
         ),
     )
-    for ind in eachindex(ranks)
+    for ind in eachindex(ms)
         df = dataframes[ind]
         df = df[data_start .< df[!, "time"] .< data_end, :]
-        rank = ranks[ind]
+        m = ms[ind]
         plot!(p, 
             df[!, "time"], 
             abs.(df[!, observable] .- df[1, observable]) .+ 1e-30,
-            label="r = $rank",
+            label="m = $m",
             #label="",
             linewidth=3,
-            color=df[:, "rank"]
+            color=ind
         )
     end
     #=for rank in 5:15
@@ -96,10 +98,8 @@ end
 
 df1 = dataframes[1]
 
-inner_product_norm_plots = map(3:7) do power
+inner_product_norm_plots = map(2:7) do power
 
-    rows = data_start .< df1[!, "time"] .< data_end
-    
     p =  plot(
         title="Error in $power" * (power == 3 ? "rd" : "th") * " moment",
         size=(900,600),
@@ -114,18 +114,27 @@ inner_product_norm_plots = map(3:7) do power
         ),
     )
 
-    plot!(p, 
-        df[rows, "time"],
-        abs.(df1[rows, "moment $power continuity error"]),
-        label="Inner product error representation",
-        linewidth=3
-    )
-    plot!(p, 
-        df[rows, "time"],
-        df1[rows, "moment $power norm error"],
-        label="Pessimistic norm estimate",
-        linewidth=3
-    )
+    for ind in eachindex(ms)
+        df = dataframes[ind]
+        df = df[data_start .< df[!, "time"] .< data_end, :]
+        m = ms[ind]
+
+        plot!(p, 
+            df[:, "time"],
+            abs2.(df[:, "moment $power continuity error"]),
+            label="Error, m = $m",
+            linewidth=3,
+            color=ind
+        )
+        plot!(p, 
+            df[:, "time"],
+            df[:, "moment $power norm error"],
+            label="Pessimistic estimate, m = $m",
+            linewidth=3,
+            color=ind,
+            style=:dash
+        )
+    end
 
     p
 end
